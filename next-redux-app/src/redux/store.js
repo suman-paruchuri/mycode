c05973ba9dc78eb4d57d456f141e39917c9a98db
suman-paruchuri <paruchuri.java@gmail.com>
2024-10-22 18:12:04 +0000

    training day 2

--- next-redux-app/src/redux/store.js
+++ next-redux-app/src/redux/store.js
@@ -1,11 +1,18 @@
 // src/redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './slices';
+import { counterReducer, dataReducer } from './slices'; // Import both reducers
 
-// Configure and export the Redux store
 export const store = configureStore({
   reducer: {
-    counter: counterReducer, // Add your slices here
+    counter: counterReducer,  // Counter slice
+    data: dataReducer,        // Data slice for SSR and SSG
   },
+  // The middleware key should return an array of middleware functions
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      // If you need to customize any middleware options, do so here
+      thunk: true, // This is true by default, so it's optional to specify
+    }),
 });
+
 
